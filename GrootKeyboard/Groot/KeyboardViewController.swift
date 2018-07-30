e97c3fb88b3ad859a1398e359efd4bcad8739880
rulaz15 <A01260402@itesm.mx>
2018-07-30 12:18:38 -0500

    groot keyboard
    i'm groot

---
+++ GrootKeyboard/Groot/KeyboardViewController.swift
@@ -0,0 +1,69 @@
+//
+//  KeyboardViewController.swift
+//  Groot
+//
+//  Created by Raúl Torres on 27/07/18.
+//  Copyright © 2018 ISA. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardViewController: UIInputViewController {
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        
+        // Add custom view sizing constraints here
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let grootBtn = UIButton(type: .system)
+        grootBtn.setBackgroundImage(UIImage(named: "groot.jpg"), for: .normal)
+        grootBtn.frame = CGRect(x: 150, y: 40, width: 100, height: 120)
+        grootBtn.addTarget(self, action: #selector(grootTapped), for: .touchUpInside)
+        
+        view.addSubview(grootBtn)
+        
+        
+        // Perform custom UI setup here
+        self.nextKeyboardButton = UIButton(type: .system)
+        
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+        
+        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    }
+    
+    override func textWillChange(_ textInput: UITextInput?) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+    
+    override func textDidChange(_ textInput: UITextInput?) {
+        // The app has just changed the document's contents, the document context has been updated.
+        
+        var textColor: UIColor
+        let proxy = self.textDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
+            textColor = UIColor.white
+        } else {
+            textColor = UIColor.black
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, for: [])
+    }
+
+    @objc func grootTapped(){
+        let proxy = textDocumentProxy as? UITextDocumentProxy
+        proxy?.insertText("i'm groot!")
+    }
+    
+}
---
+++ GrootKeyboard/GrootKeyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrootKeyboard
+//
+//  Created by Raúl Torres on 27/07/18.
+//  Copyright © 2018 ISA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
